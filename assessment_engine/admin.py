cbdcc87750056b2d6932746e5b6f13a274b9ceae
Ashis Rao <ashisrao1999@gmail.com>
2022-06-24 20:19:52 +0530

    registered models with admin module

--- assessment_engine/admin.py
+++ assessment_engine/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+admin.site.register(models.Standard)
+admin.site.register(models.Subject)
+admin.site.register(models.QuestionBank)
+admin.site.register(models.Question)
+admin.site.register(models.Answer)
